3695bc63f23339228c20ddbd2dded527351c5705
N-o-Z <ozery.nir@gmail.com>
2024-03-02 11:11:42 +0200

    Update src/main/java/io/lakefs/iceberg/catalog/Util.java
    Co-authored-by: Ariel Shaqed (Scolnicov) <ariel.shaqed@gmail.com>

--- src/main/java/io/lakefs/iceberg/catalog/Util.java
+++ src/main/java/io/lakefs/iceberg/catalog/Util.java
@@ -10,7 +10,7 @@ public class Util {
      * Given a full lakeFS URI, return the path relative to the repository root.
      * For example, given <code>lakefs://example-repo/main/a/b/c</code>, return <code>a/b/c</code>.
      */
-    public static String GetPathFromURL(String lakeFSLocation){
+    public static String getPathFromURL(String lakeFSLocation){
         // return sub-string after lakeFS ref
         return StringUtils.substring(lakeFSLocation, StringUtils.ordinalIndexOf(lakeFSLocation, "/", 4) + 1);
     }
